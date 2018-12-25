bc1b794335bf49f8df7618eee6455b4831177da3
KunduzA <registanova.kunduz@gmail.com>
2018-12-26 00:02:49 +0600

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\students_hotstart-django2\students_hotstart-django2\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
